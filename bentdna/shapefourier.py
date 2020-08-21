8a0dd7d1d75625d770b1fcc5cb056326cb060c70
Yizao <yizaochen@gmail.com>
2020-08-21 20:17:06 +0800

    Unit Check

--- bentdna/shapefourier.py
+++ bentdna/shapefourier.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, rename
 import pandas as pd
 import numpy as np
 from bentdna.miscell import check_dir_exist_and_make
@@ -17,18 +17,22 @@ class ShapeAgent:
         'ctct_21mer': (3, 17), 'tgtg_21mer': (3, 17), '500mm': (3, 12),
         'only_cation': (3, 12), 'mgcl2_150mm': (3, 12)}
 
-    def __init__(self, workfolder, host):
+    def __init__(self, workfolder, host, bp_id_first=None, bp_id_last=None):
         self.host = host
         self.rootfolder = path.join(workfolder, host)
         self.df_folder = path.join(self.rootfolder, 'l_theta')
         self.an_folder = path.join(self.rootfolder, 'an_folder')
         self.n_bead = self.d_n_bp[host]
-        self.bp_id_first = self.start_end[host][0]
-        self.bp_id_last = self.start_end[host][1]
+        self.bp_id_first = self.__set_bp_id_first(bp_id_first)
+        self.bp_id_last = self.__set_bp_id_last(bp_id_last)
         self.df_name = path.join(self.df_folder, f'l_modulus_theta_{self.n_bead}_beads.csv')
         self.df = None
 
         self.__check_and_make_folders()
+
+    def get_appr_L(self):
+        # Unit: nm
+        return 0.34 * (self.bp_id_last - self.bp_id_first)
         
     def read_l_modulus_theta(self):
         self.df =  pd.read_csv(self.df_name)
@@ -44,12 +48,12 @@ class ShapeAgent:
 
         df_an = pd.DataFrame(d_result)
         df_an = df_an[columns]
-        f_out = path.join(self.an_folder, f'an_{n_begin}_{n_end}.csv')
+        f_out = path.join(self.an_folder, f'an_{n_begin}_{n_end}_bpfirst{self.bp_id_first}_bplast{self.bp_id_last}.csv')
         df_an.to_csv(f_out, index=False)
         return df_an
 
     def read_df_an(self, n_begin, n_end):
-        f_in = path.join(self.an_folder, f'an_{n_begin}_{n_end}.csv')
+        f_in = path.join(self.an_folder, f'an_{n_begin}_{n_end}_bpfirst{self.bp_id_first}_bplast{self.bp_id_last}.csv')
         df_an = pd.read_csv(f_in)
         return df_an
 
@@ -63,17 +67,30 @@ class ShapeAgent:
         return df2
 
     def get_an(self, n, df_filter):
-        L = self.__get_L(df_filter)
+        L = self.__get_L(df_filter) # unit: angstrom
+        L_nm = L / 10 # unit: nm
         delta_s_list = self.__get_delta_s_list(df_filter)
         theta_list = self.__get_theta_list(df_filter)
         s_mid_list = self.__get_s_mid_list(df_filter)
-        scale_factor = np.sqrt(2/L)
+        scale_factor = np.sqrt(2/L_nm)
         summation = 0
         for delta_s, theta, s_mid in zip(delta_s_list, theta_list, s_mid_list):
             in_cos_term = n * np.pi / L
             cos_term = np.cos(in_cos_term * s_mid)
-            summation += delta_s * theta * cos_term
+            summation += delta_s * 0.1 * theta * cos_term
         return scale_factor * summation
+
+    def __set_bp_id_first(self, bp_id_first):
+        if bp_id_first is None:
+            return self.start_end[self.host][0]
+        else:
+            return bp_id_first
+
+    def __set_bp_id_last(self, bp_id_last):
+        if bp_id_last is None:
+            return self.start_end[self.host][1]
+        else:
+            return bp_id_last
 
     def __initialize_an_d_result(self, n_begin, n_end):
         d_result = dict()
